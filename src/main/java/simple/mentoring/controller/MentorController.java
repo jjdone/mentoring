16889253b372e091ab421106af1589ae693fcc01
jjdone <ehdlfaos@gmail.com>
2023-02-04 13:14:05 +0900

    멘토 회원가입 비밀번호 보안 추가

--- src/main/java/simple/mentoring/controller/MentorController.java
+++ src/main/java/simple/mentoring/controller/MentorController.java
@@ -2,9 +2,14 @@ package simple.mentoring.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import simple.mentoring.domain.Mentor;
+import simple.mentoring.dto.MentorSignupDto;
 import simple.mentoring.service.MentorService;
 
 @Controller
@@ -19,6 +24,23 @@ public class MentorController {
     public String addForm() {
         log.info("Mentor Controller: addForm");
         return "mentors/addForm";
+    }
+
+    @PostMapping("/add")
+    public String signup(@ModelAttribute MentorSignupDto mentorSignupDto) {
+        log.info("Mentor Controller: signup");
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        Mentor mentor = Mentor.builder()
+                .email(mentorSignupDto.getEmail())
+                .password(encoder.encode(mentorSignupDto.getPassword()))
+                .name(mentorSignupDto.getName())
+                .phone(mentorSignupDto.getPhone())
+                .rating(0l)
+                .build();
+
+        mentorService.join(mentor);
+
+        return "redirect:/mentors/login";
     }
 
     @GetMapping("/login")
