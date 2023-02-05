b2c2cf349f9249365c72e5e0541b296f68210cd1
jjdone <ehdlfaos@gmail.com>
2023-02-05 13:44:45 +0900

    멘토링 기능 추가
    등록, 상세화면 보기, 멘토링 목록 보기 기능 추가

--- src/main/java/simple/mentoring/controller/MentoringController.java
+++ src/main/java/simple/mentoring/controller/MentoringController.java
@@ -1,2 +1,60 @@
-package simple.mentoring.controller;public class MentoringController {
+package simple.mentoring.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import simple.mentoring.domain.Mentoring;
+import simple.mentoring.dto.MentoringDto;
+import simple.mentoring.service.MentoringService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/mentorings")
+@Slf4j
+public class MentoringController {
+
+    private final MentoringService mentoringService;
+
+    @GetMapping()
+    public String mentoringList(Model model) {
+        log.info("Mentoring Controller: mentoringList");
+        List<Mentoring> mentorings = mentoringService.findMentorings();
+        model.addAttribute("mentorings", mentorings);
+        return "mentorings/mentoringList";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        log.info("Mentoring Controller: addForm");
+        model.addAttribute("form", new MentoringDto());
+        return "mentorings/addForm";
+    }
+
+    @PostMapping("/add")
+    public String add(MentoringDto mentoringDto) {
+        log.info("Mentoring Controller: add");
+        mentoringService.addMentoring(mentoringDto);
+        return "redirect:/mentorings";
+    }
+
+    @GetMapping("/{mentoringId}")
+    public String mentoringDetail(@PathVariable("mentoringId") Long mentoringId, Model model) {
+        log.info("Mentoring Controller: mentoringDetail");
+        Mentoring mentoring = mentoringService.findOne(mentoringId);
+
+        MentoringDto mentoringDto = new MentoringDto();
+        mentoringDto.setTitle(mentoring.getTitle());
+        mentoringDto.setContent(mentoring.getContent());
+        mentoringDto.setEmail(mentoring.getMentor().getEmail());
+
+        model.addAttribute("form", mentoringDto);
+        return "mentorings/detail";
+    }
 }
--- src/main/java/simple/mentoring/service/MentoringService.java
+++ src/main/java/simple/mentoring/service/MentoringService.java
@@ -1,2 +1,49 @@
-package simple.mentoring.service;public class MentoringService {
+package simple.mentoring.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import simple.mentoring.domain.Mentor;
+import simple.mentoring.domain.Mentoring;
+import simple.mentoring.dto.MentoringDto;
+import simple.mentoring.repository.MentorRepository;
+import simple.mentoring.repository.MentoringRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MentoringService {
+
+    private final MentoringRepository mentoringRepository;
+    private final MentorRepository mentorRepository;
+
+    @Transactional
+    public void saveMentoring(Mentoring mentoring) {
+        mentoringRepository.save(mentoring);
+    }
+
+    public List<Mentoring> findMentorings() {
+        return mentoringRepository.findAll();
+    }
+
+    public Mentoring findOne(Long mentoringId) {
+        return mentoringRepository.findById(mentoringId).get();
+    }
+
+    public Long addMentoring(MentoringDto mentoringDto) {
+        Mentor mentor = mentorRepository.findByEmail(mentoringDto.getEmail());
+
+        Mentoring mentoring = Mentoring.builder()
+                .title(mentoringDto.getTitle())
+                .content(mentoringDto.getContent())
+                .createDate(LocalDateTime.now())
+                .mentee(null)
+                .build();
+        mentoring.setMentor(mentor);
+
+        mentoringRepository.save(mentoring);
+        return mentoring.getId();
+    }
 }
