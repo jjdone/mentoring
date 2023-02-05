d222f7f6e31fae2d1829d443dc820a5023d0d3d3
jjdone <ehdlfaos@gmail.com>
2023-02-05 11:51:19 +0900

    멘티 엔티티 조건 추가

--- src/main/java/simple/mentoring/domain/Mentee.java
+++ src/main/java/simple/mentoring/domain/Mentee.java
@@ -2,6 +2,7 @@ package simple.mentoring.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,14 +11,20 @@ import javax.persistence.Id;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Mentee {
 
     @Id @GeneratedValue
     @Column(name = "mentee_id")
     private Long id;
 
+    @Column(length = 100, nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(length = 30, nullable = false)
     private String name;
     private String phone;
 
